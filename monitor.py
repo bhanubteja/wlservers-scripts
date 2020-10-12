508eb431b02052a20d039cb0eac20fb1ae78ef95
bhanubteja <bteja28@gmail.com>
2020-10-12 13:25:42 +0530

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,15 @@
+import thread
+import time
+def checkHealth(serverName):
+  while 1:
+    slBean = getSLCRT(serverName)
+    status = slBean.getState()
+    print 'Status of Managed Server is '+status
+    if status != "RUNNING":
+      print 'Starting server '+serverName
+      start(serverName, block="true")
+    time.sleep(5)
+def getSLCRT(svrName):
+    domainRuntime()
+    slrBean = cmo.lookupServerLifecycleRuntime(svrName)
+    return slcBean
